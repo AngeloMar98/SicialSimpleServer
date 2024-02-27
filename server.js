9538c93fad21eb3c173d3a0631896c0b00bc9bff
angelomar <angelomarcia98@gmail.com>
2024-02-27 22:05:14 +0100

    added last assignment

--- server.js
+++ server.js
@@ -4,62 +4,63 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url);
-  if (req.url !== "/favicon.ico") {
-    // log the requested method and the url
-    console.log(`Request method: ${req.method}\nRequest URL: ${req.url} `);
+  // return if the favicon.ico is requested, to avoid errors
+  if (req.url === "/favicon.ico") return;
+  // log the requested method and the url
+  console.log(`Request method: ${req.method}\nRequest URL: ${req.url} `);
 
-    // the type of content we'll give back, being a site we deliver the HTML pages
-    res.setHeader("Content-Type", `text/html`);
+  // the type of content we'll give back, being a site we deliver the HTML pages
+  res.setHeader("Content-Type", `text/html`);
 
-    let path = "./views/";
+  let path = "./views/";
 
-    // depending on the url we have a different page, if there is no case can we can handle
-    // we throw the 404 error
+  // depending on the url we have a different page, if there is no case can we can handle
+  // we throw the 404 error
 
-    switch (req.url) {
-      case "/":
-      case "/index.html":
-        path += "index.html";
-        res.statusCode = 200;
-        break;
-      case "/about":
-      case "/about.html":
-        path += "about.html";
-        res.statusCode = 200;
-        break;
-      case "/projects":
-      case "/projects.html":
-        path += "projects.html";
-        res.statusCode = 200;
-        break;
-      case "/assignments":
-      case "/assignments.html":
-        path += "assignments.html";
-        res.statusCode = 200;
-        break;
-      default:
-        path += "404.html";
-        res.statusCode = 404;
-        break;
-    }
-
-    // fs searches the file, it'll throw an error if such file doesn't exist,
-    // or store in data if it does
-    fs.readFile(`${path}`, (err, data) => {
-      // the error is handled, it ends
-      if (err) {
-        console.log(err);
-        res.end();
-      } else {
-        // lastly if the page does exist we write and end the request with a positive response
-        res.write(data);
-        res.end();
-      }
-    });
+  switch (req.url) {
+    case "/":
+    case "/index.html":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+    case "/about.html":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/projects":
+    case "/projects.html":
+      path += "projects.html";
+      res.statusCode = 200;
+      break;
+    case "/assignments":
+    case "/assignments.html":
+      path += "assignments.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
   }
+
+  // fs searches the file, it'll throw an error if such file doesn't exist,
+  // or store in data if it does
+  fs.readFile(`${path}`, (err, data) => {
+    // the error is handled, it ends
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      // lastly if the page does exist we write and end the request with a positive response
+      res.write(data);
+      res.end();
+    }
+  });
 });
 
 server.listen(3000, "localhost", () => {
-  console.log("Listening for requests for the HTTP server, port 3000");
+  console.log(
+    "Listening for requests for the HTTP server, port 3000.\n Go to http://localhost:3000/ "
+  );
 });
